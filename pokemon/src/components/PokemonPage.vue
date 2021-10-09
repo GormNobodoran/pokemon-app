471f868ef49ad485907c139cb3711060e47718a8
CodigoMorrazo <ipedrosacip@gmail.com>
2021-10-09 10:45:59 +0200

    Added basic structure

---
+++ pokemon/src/components/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>¿Quién es este pokémon?</h1>
+
+  <pokemon-picture/>
+
+  <pokemon-options/>
+</template>
+
+<script>
+import PokemonPicture from './PokemonPicture';
+import PokemonOptions from './PokemonOptions';
+
+export default {
+  name: "PokemonPage.vue",
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  }
+}
+</script>
---
+++ pokemon/src/components/PokemonPicture.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pokemon-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         class="hidden-pokemon"
+         alt="pokémon">
+
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         class="fade-in"
+         alt="pokémon">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonPicture.vue"
+}
+</script>
+
+<style scoped>
+  .pokemon-container {
+    height: 200px;
+  }
+  img {
+    height: 200px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+  }
+  .hidden-pokemon {
+    filter: brightness(0);
+  }
+  .hidden-pokemon {
+    filter: brightness(0);
+  }
+</style>
